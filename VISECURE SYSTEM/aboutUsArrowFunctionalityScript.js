e203d584e4d444b60f7b85fce3e9abbb5e5443cc
DarkyKrad <darkykrady12@gmail.com>
2023-11-28 17:14:07 +0800

    Fixing About Us

--- VISECURE SYSTEM/aboutUsArrowFunctionalityScript.js
+++ VISECURE SYSTEM/aboutUsArrowFunctionalityScript.js
@@ -1,48 +1,71 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var aboutUsSubText = document.querySelector('.aboutUsSubText');
-    var mission1Container = document.querySelector('.mission1Container');
-    var whyUsContainer = document.querySelector('.whyUsContainer');
-    var contactUsContainer = document.querySelector('.contactUSContainer');
-    var mission2Container = document.querySelector('.mission2Container');
+// document.addEventListener('DOMContentLoaded', function () {
+//     var aboutUsSubText = document.querySelector('.aboutUsSubText');
+//     var mission1Container = document.querySelector('.mission1Container');
+//     var whyUsContainer = document.querySelector('.whyUsContainer');
+//     var contactUsContainer = document.querySelector('.contactUSContainer');
+//     var mission2Container = document.querySelector('.mission2Container');
 
-    var arrow1 = document.querySelector('.arrow1');
-    var arrow2 = document.querySelector('.arrow2');
+//     var arrow1 = document.querySelector('.arrow1');
+//     var arrow2 = document.querySelector('.arrow2');
 
-    var components = [aboutUsSubText, mission1Container, whyUsContainer, contactUsContainer, mission2Container];
-    var currentIndex = 0; // Start from the first component
+//     var components = [aboutUsSubText, mission1Container, whyUsContainer, contactUsContainer, mission2Container];
+//     var currentIndex = 0; // Start from the first component
 
-    // Function to hide all components
-    function hideAllComponents() {
-        components.forEach(function (component) {
-            component.classList.add('hidden');
-        });
-    }
+//     // Function to hide all components
+//     function hideAllComponents() {
+//         components.forEach(function (component) {
+//             component.classList.add('hidden');
+//         });
+//     }
 
-    hideAllComponents(); // Hide all components initially
+//     hideAllComponents(); // Hide all components initially
 
-    function toggleVisibility(element) {
-        element.classList.toggle('hidden');
-    }
+//     function toggleVisibility(element) {
+//         element.classList.toggle('hidden');
+//     }
 
-    function showNextComponent() {
-        if (currentIndex < components.length) {
-            toggleVisibility(components[currentIndex]);
-            currentIndex++;
-        }
-    }
+//     function showNextComponent() {
+//         if (currentIndex < components.length) {
+//             toggleVisibility(components[currentIndex]);
+//             currentIndex++;
+//         }
+//     }
 
-    function hideCurrentComponent() {
-        if (currentIndex > 0) {
-            currentIndex--;
-            toggleVisibility(components[currentIndex]);
-        }
-    }
+//     function hideCurrentComponent() {
+//         if (currentIndex > 0) {
+//             currentIndex--;
+//             toggleVisibility(components[currentIndex]);
+//         }
+//     }
 
-    arrow1.addEventListener('click', function () {
-        showNextComponent();
+//     arrow1.addEventListener('click', function () {
+//         showNextComponent();
+//     });
+
+//     arrow2.addEventListener('click', function () {
+//         hideCurrentComponent();
+//     });
+// });
+
+const slides = document.querySelectorAll('.slides p');
+const radios = document.querySelectorAll('.slideshow input[type="radio"]');
+let currentIndex = 0;
+
+function showSlide(index) {
+    slides.forEach(slide => {
+        slide.style.display = 'none';
     });
 
-    arrow2.addEventListener('click', function () {
-        hideCurrentComponent();
+    slides[index].style.display = 'block';
+    radios[index].checked = true;
+    currentIndex = index;
+}
+
+radios.forEach(radio => {
+    radio.addEventListener('change', function() {
+        const index = parseInt(this.value);
+        showSlide(index);
     });
 });
+
+showSlide(currentIndex);
